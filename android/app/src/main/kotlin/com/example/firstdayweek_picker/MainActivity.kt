6530f9dd1c26ee4b5e84ec3b484b86e154f6db7b
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-12-29 17:03:31 +0530

    FLUT-3306 - Sample and screenshot added

---
+++ android/app/src/main/kotlin/com/example/firstdayweek_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firstdayweek_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(FirstDayOfWeek());
+
+class FirstDayOfWeek extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            body: Card(
+          margin: const EdgeInsets.fromLTRB(50, 150, 50, 150),
+          child: SfDateRangePicker(
+            monthViewSettings:
+                DateRangePickerMonthViewSettings(firstDayOfWeek: 3),
+          ),
+        )));
+  }
+}
